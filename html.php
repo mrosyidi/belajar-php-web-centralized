5bc7d4b54b4f5116e6dad580cc67033bfdc79455
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-07-18 11:35:50 +0700

    update html.php menggunakan tanda <?=

--- html.php
+++ html.php
@@ -5,9 +5,9 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title><?php echo $title ?></title>
+    <title><?= $title ?></title>
   </head>
   <body>
-    <h1><?php echo $body ?></h1>
+    <h1><?= $body ?></h1>
   </body>
 </html>
