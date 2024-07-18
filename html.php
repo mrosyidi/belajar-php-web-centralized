7b07f9bce87dd51e97323418f39a79f6010d466d
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-07-18 11:34:56 +0700

    add html.php

---
+++ html.php
@@ -0,0 +1,13 @@
+<?php
+  $title = "Hello PHP dan HTML";
+  $body = "Hello PHP dan HTML";
+?>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo $title ?></title>
+  </head>
+  <body>
+    <h1><?php echo $body ?></h1>
+  </body>
+</html>
