ef6cb203b6af4bea62128ec056c656bba0c984cc
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-07-25 08:51:26 +0700

    update upload.php memindah file upload

--- upload.php
+++ upload.php
@@ -6,6 +6,7 @@
     $file_size = $_FILES['upload_file']['size'];
     $file_tmp_name = $_FILES['upload_file']['tmp_name'];
     $file_error = $_FILES['upload_file']['error'];
+    move_uploaded_file($file_tmp_name, __DIR__ . '/file/' . $file_name);
   }
 ?>
 
