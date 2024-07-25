d4138c3ae66c83471ea20e2efb4a03098203a55a
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-07-25 13:35:46 +0700

    add download.php

---
+++ download.php
@@ -0,0 +1,12 @@
+<?php
+
+  if(isset($_GET['key']) && $_GET['key'] == 'rahasia'){
+    header('Content-Disposition: attachment; filename="profile.php"');
+    header('Content-Type: image/png');
+    readfile(__DIR__ . '/file/profile.png');
+    exit();
+  }else
+  {
+    echo "Invalid Link";
+    exit();
+  }
